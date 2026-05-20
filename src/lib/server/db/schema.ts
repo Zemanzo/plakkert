5be0c37d2a0bc5eb00160bfde88394db1bff077e
Merlijn van der Kamp <github@merlijn.me>
2026-05-20 16:08:11 +0200

    add notes to db schema

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,11 +1,21 @@
 import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
+import { user } from './auth.schema';
 
-export const task = sqliteTable('task', {
+export const notes = sqliteTable('notes', {
 	id: text('id')
 		.primaryKey()
 		.$defaultFn(() => crypto.randomUUID()),
-	title: text('title').notNull(),
-	priority: integer('priority').notNull().default(1)
+	user: text('user_id').references(() => user.id).notNull(),
+	title: text('title'),
+	content: text('content'),
+	createdAt: integer('created_at', { mode: 'timestamp' })
+		.notNull()
+		.$defaultFn(() => new Date()),
+	updatedAt: integer('updated_at', { mode: 'timestamp' })
+		.notNull()
+		.$defaultFn(() => new Date()),
+	archived: integer('archived').notNull().$default(() => 0),
+	style: text('style')
 });
 
 export * from './auth.schema';
