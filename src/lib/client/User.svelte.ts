dc9366f8195fd0a9a6538abc4f4b8a4fa072c2c6
Merlijn van der Kamp <github@merlijn.me>
2026-06-20 16:09:14 +0200

    fix local user state management between pages; adjust editor styles

--- src/lib/client/User.svelte.ts
+++ src/lib/client/User.svelte.ts
@@ -1,18 +1,21 @@
 // userState.svelte.ts
 import { liveQuery } from 'dexie';
 import { db, type User } from './Database';
-import { page } from '$app/state';
 
 class LocalUser {
 	#user = $state<User | null>(null);
 	#isLoading = $state(true);
+	#subscription: { unsubscribe: () => void } | null = null;
 
-	constructor() {
-		const user = liveQuery(() =>
-			page.data?.user?.id ? db.users.get(page.data.user.id) : undefined
-		);
-
-		user.subscribe((value) => {
+	init(userId: string | undefined) {
+		this.#subscription?.unsubscribe();
+		if (!userId) {
+			this.#isLoading = false;
+			this.#user = null;
+			return;
+		}
+		this.#isLoading = true;
+		this.#subscription = liveQuery(() => db.users.get(userId)).subscribe((value) => {
 			this.#isLoading = false;
 			this.#user = value ?? null;
 		});
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,16 @@
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
 	import Header from '$lib/client/Header.svelte';
+	import { page } from '$app/state';
 	import { onMount } from 'svelte';
 	import { localUser } from '$lib/client/User.svelte';
 
 	let { children, data } = $props();
+
+	$effect(() => {
+		// (Re-)initialize localUser as otherwise it will lose the user state when navigating to a new page.
+		localUser.init(page.data.user?.id);
+	});
 
 	onMount(() => {
 		$effect(() => {
